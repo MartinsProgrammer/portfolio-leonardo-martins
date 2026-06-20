ab5bf3d8fd11e70c9570d1faa19d58c3ed4e4e83
Leonardo Martins <129529004+MartinsProgrammer@users.noreply.github.com>
2026-06-20 19:19:35 +0100

    versao melhorada

--- main.js
+++ main.js
@@ -68,3 +68,20 @@ if (tiltCard && window.matchMedia("(pointer: fine)").matches) {
     tiltCard.style.transform = "rotateY(0deg) rotateX(0deg)";
   });
 }
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 400){
+        backToTop.classList.add("show");
+    }else{
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
